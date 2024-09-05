67f3c5b8d40c3bd65d427fb1a3cb818564039079
shoyonp <shoyonkumar143@gmail.com>
2024-09-06 01:24:30 +0600

    function 1 done

---
+++ function.js
@@ -0,0 +1,29 @@
+const price = 54;
+
+// function declare
+function fanOffkor(){
+    console.log('stand up');
+    console.log('walk towards the switch');
+    console.log('click the switch to off of the fan');
+}
+
+// call the function
+fanOffkor();
+
+
+// function name(){
+// 
+// }
+function brushTeeht(){
+    console.log('pick up the brush');
+    console.log('add paste');
+    console.log('2 min left and right majo');
+}
+
+// call the function
+brushTeeht();
+console.log('--------------')
+console.log('--------------')
+console.log('--------------')
+console.log('tomato')
+brushTeeht();
