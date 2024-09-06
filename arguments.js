b1eb4cda4c479e79a199b2587caa61fa98cf0abe
shoyonp <shoyonkumar143@gmail.com>
2024-09-06 12:12:50 +0600

    argument done

---
+++ arguments.js
@@ -0,0 +1,26 @@
+function doubleIt(number){
+    const doubled = number * 2;
+    console.log(number, doubled);
+}
+
+console.log('i will call the funtion');
+doubleIt(43);
+console.log('-----------');
+doubleIt(72);
+console.log('------------');
+doubleIt(713);
+const number = 34;
+doubleIt(number);
+const money = 112;
+doubleIt(money);
+
+
+// 
+function difference (num1, num2 ){
+    const diff = num1 - num2;
+    console.log(num1 , num2, 'difference is', diff);
+}
+const fatherAge =  40;
+const myAge = 10;
+
+difference(fatherAge, myAge);
